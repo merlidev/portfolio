33b0a0acea08819dc6563c007d908e856fe38b56
merlidev <98073523+merlidev@users.noreply.github.com>
2022-02-12 16:48:21 -0600

    changes

--- projecta/android/app/src/main/kotlin/com/example/projecta/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projecta
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projecta/lib/constants/app_constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-final Color mainHexColor = Color(0xff1C1C1C);
-final Color accentHexColor = Color(0xffFCC91C);
